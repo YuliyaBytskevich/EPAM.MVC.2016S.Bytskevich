2d55fe2980c8936e2cbfda3a7900c153b04fbb13
Yuliya Bytskevich <yuliya.bytskevich@gmail.com>
2016-09-07 11:26:38 +0300

    day3.fractions HW added

---
+++ Day3.Fractions/Day3.HW.Fractions/Models/Side.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day3.HW.Fractions.Models
+{
+    public enum Side { Light, Dark }
+}
---
+++ Day3.Fractions/Day3.HW.Fractions/scripts/logic.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    var $headerWithFraction = $("h1");
+    var $fieldContainer = $(".container.body-content");
+    var $menuContainer = $(".navbar");
+
+    if ($headerWithFraction.text() == "Fraction: Light") {
+        $fieldContainer.addClass("light-field");
+    }
+
+});
